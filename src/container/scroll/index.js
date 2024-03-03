d866954b3b31549ccf82596ed82bae455dd1941c
unknown <makskanevski12@gmail.com>
2024-03-03 02:46:12 +0100

    nav-scroll add

---
+++ src/container/scroll/index.js
@@ -0,0 +1,28 @@
+let isDisplay = false
+
+setInterval(()=>{
+    if(
+        window.scrollY > window.innerHeight&&
+        isDisplay === false
+    ){
+        isDisplay = true
+        window.buttonScroll.style.display = 'flex'
+        return
+    }
+    if(
+        window.scrollY <= window.innerHeight&&
+        isDisplay === true
+    ){
+        isDisplay = false
+        window.buttonScroll.style.display = 'none'
+        return
+    }
+}, 500)
+
+
+window.buttonScroll.onclick = ()=>
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
